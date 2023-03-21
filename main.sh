716d0430fd7ad3ffedb3ddc43c93d33a79502803
Ward Nakchbandi <hotrod.master@hotmail.com>
2023-03-21 16:09:58 +0300

    a fucking theme shouldn't even take that long

--- main.sh
+++ main.sh
@@ -7,19 +7,19 @@ add-apt-repository ppa:pikaos/pika
 add-apt-repository ppa:kubuntu-ppa/backports
 # Clone Upstream
 sudo apt install -y libsass1 sassc
+mkdir -p orchis-theme
 git clone https://github.com/vinceliuice/Orchis-kde
 cd ./Orchis-kde
 patch -Np1 -i ../patches/kde.patch
 ./install.sh
-cp -rvf ./orchis-theme-kde ../debian/
+cp -rvf ./orchis-theme-kde ../orchis-theme/
 cd ../
 #git clone https://github.com/vinceliuice/Orchis-theme
 cd ./Orchis-theme
 #mkdir -p ./orchis-theme-gtk/usr/share/themes
 #./install.sh -d ./orchis-theme-gtk/usr/share/themes -t all  --shell 42 --tweaks submenu
-cp -rvf ./orchis-theme-gtk ../debian/
+cp -rvf ./orchis-theme-gtk ../orchis-theme/
 cd ../
-mkdir -p orchis-theme
 cp -rvf ./debian ./orchis-theme/
 cd  orchis-theme
 
