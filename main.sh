0de61914d67923c5f8ca5f3827c817e7ff77621d
Ward Nakchbandi <hotrod.master@hotmail.com>
2023-03-21 15:56:29 +0300

    no build gtk

--- main.sh
+++ main.sh
@@ -1,5 +1,3 @@
-#! /bin/bash
-set -e
 # Add dependent repositories
 wget -q -O - https://ppa.pika-os.com/key.gpg | sudo apt-key add -
 add-apt-repository https://ppa.pika-os.com
@@ -13,10 +11,10 @@ patch -Np1 -i ../patches/kde.patch
 ./install.sh
 cp -rvf ./orchis-theme-kde ../debian/
 cd ../
-git clone https://github.com/vinceliuice/Orchis-theme
+#git clone https://github.com/vinceliuice/Orchis-theme
 cd ./Orchis-theme
-mkdir -p ./orchis-theme-gtk/usr/share/themes
-./install.sh -d ./orchis-theme-gtk/usr/share/themes -t all  --shell 42 --tweaks submenu
+#mkdir -p ./orchis-theme-gtk/usr/share/themes
+#./install.sh -d ./orchis-theme-gtk/usr/share/themes -t all  --shell 42 --tweaks submenu
 cp -rvf ./orchis-theme-gtk ../debian/
 cd ../
 mkdir -p orchis-theme
